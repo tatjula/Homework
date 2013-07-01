44feff231882aa27d4d72a853ec2005b4a808f2f
Tatyana <tat@tatyanahjula.com>
2013-07-01 12:13:14 -0400

    second try at homework 2

--- js/assignment_01.js
+++ js/assignment_01.js
@@ -1,43 +1,46 @@
+// create an oject within a string, include a name and level of completion
+
 var groceries = [{
-            "name": "eggs",
-            "complete": true
-        },  {
-            "name": "bread",
-            "complete": true
-        },  {
-            "name": "milk",
-            "complete": true
-        },  { 
-            "name": "paper towles",
-        }, {
-            "name": "monkeys",
-            "complete": false
-        }];
+        "name": "eggs",
+        "complete": true
+    }, {
+        "name": "bread",
+        "complete": true
+    }, {
+        "name": "milk",
+    }, {
+        "name": "paper towels",
+        "complete": true
+    }, {
+        "name": "monkeys",
+        "complete": false
+    } ];
+    
+    
+// create a loop that prints out the above string
+
+for (var i=0; i < groceries.length; i ++) {
+    try {
+        printGroceries(groceries[i]);
+        } catch (error) {
+            console.log(error);
+        }    
+}
+
+// create a function defining printGroceries
 
 document.write("<ul>");
-for(var i = 0; i < groceries.length; i++) {
-    document.write("<li>" + groceries[i].name + " ");
-    document.write(groceries[i].complete + " ");
+function printGroceries (items) {
+    if(items.complete === true) {
+        document.write("<li class='complete'>" + items.name + "</li>");
+    } else if(items.complete === false) {
+        document.write("<li>" + items.name + "</li>");
+    } 
+    
+    if(typeof items.name === "undefined") {
+        throw "Aren't you forgetting something?"
+    } else if (typeof items.complete === "undefined") {
+        throw "Did you get everything you need?"
     }
+};
 document.write("</ul>");
-
-for(var i = 0; i < groceries.length; i++) {
-    try {
-       print(groceries[i]);
-    } catch (error) {
-        document.write("<p>" + error + "</p>");
-    }
-}
-
-function print(groceryItem) {
-    if(typeof groceryItem.name == "undefined") {
-        throw "Did you forget to write down what you needed?"
-    } else if (typeof groceryItem.complete == "undefined") {
-        throw "Your forgot to pick up an item!"
-    } else {
-        console.log("All done.");
-    }
-}
-
-
- 
