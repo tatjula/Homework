8881e7944fde4e71fd0633869a8e0eca10adff96
Tatyana <tat@tatyanahjula.com>
2013-07-02 19:49:05 -0400

    second attempt at First Draft of Homework Assignment 3

--- js/assignment_01.js
+++ js/assignment_01.js
@@ -1,56 +1,54 @@
-// use jquery to produce HTML
+$(document).ready(function() {
 
-function generateDOM() {
 
-    var $h1 = $("<h1/>");
-    var $ul = $("<ul/>");
-    var $li;
-        
 // create an oject within a string, include a name and level of completion
 
-var groceries = [{
-        "name": "eggs",
-        "complete": true
-    }, {
-        "name": "bread",
-        "complete": true
-    }, {
-        "name": "milk",
-    }, {
-        "name": "paper towels",
-        "complete": true
-    }, {
-        "name": "monkeys",
-        "complete": false
-    } ];
-        
-    // create a loop that prints out the above string
+    var groceries = [{
+            "name": "eggs",
+            "complete": true
+        }, {
+            "name": "bread",
+            "complete": true
+        }, {
+            "name": "milk",
+        }, {
+            "name": "paper towels",
+            "complete": true
+        }, {
+            "name": "monkeys",
+            "complete": false
+        } ];
     
+// use jquery to produce HTML
+
+    var html = "<h1>Groceries</h1>";
+    html += "<ul>";
+    
+// create a loop that prints out the above string
+
     for (var i=0; i < groceries.length; i ++) {
         try {
-            var printGroceries(groceries[i]);
+            html += printGroceries(groceries[i]);
             } catch (error) {
                 console.log(error);
-            }    
+            }
     }
     
-    // create a function defining printGroceries
-    
-        function printGroceries (items) {
-            if(typeof items.name === "undefined") {
-                throw "Aren't you forgetting something?";
-            } else if (typeof items.complete === "undefined") {
-                throw "Did you get everything you need?";
-            }
-            
-            if(items.complete === true) {
-                $li = $("<li/>", {"class" : "complete"})items.name;
-            } else if(items.complete === false) {
-                $li = $("<li/>")items.name;
-            } 
-        }
-            $ul.append($li);
-    
-    $("ul").append($("li"));
+    html += "</ul>";
 
-}
+// create a function defining printGroceries
+
+    function printGroceries (items) {
+        if(typeof items.name === "undefined") {
+            throw "Aren't you forgetting something?";
+        } else if (typeof items.complete === "undefined") {
+            throw "Did you get everything you need?";
+        }
+        
+        if(items.complete === true) {
+            return("<li class='complete'>" + items.name + "</li>");
+        } else if(items.complete === false) {
+            return("<li>" + items.name + "</li>");
+        }
+    }
+});
