699c12cc14261e3c24e11ae5a17e7eacb8393137
Tatyana <tat@tatyanahjula.com>
2013-07-09 16:00:13 -0400

    Homework Assignment 4: First Draft

--- js/assignment_01.js
+++ js/assignment_01.js
@@ -1,46 +1,68 @@
 $(document).ready(function() {
-    // create an oject within a string, include a name and level of completion
-    var groceries = [{
-        "name": "eggs",
-        "complete": true
-    }, {
-        "name": "bread",
-        "complete": true
-    }, {
-        "name": "milk",
-    }, {
-        "name": "paper towels",
-        "complete": true
-    }, {
-        "name": "monkeys",
-        "complete": false
-    }];
-    // use jquery to produce HTML
+
+// a list of items that print to HTML
+// use jquery to produce HTML
+
     var html = "<h1>Groceries</h1>";
     html += "<ul>";
-    // create a loop that prints out the above string
-    for (var i = 0; i < groceries.length; i++) {
-        try {
-            html += printGroceries(groceries[i]);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    html += "</ul>";
-    $('.container').html(html);
-    $(".complete").remove();
-    // create a function defining printGroceries
 
-    function printGroceries(items) {
-        if (typeof items.name === "undefined") {
-            throw "Aren't you forgetting something?";
-        } else if (typeof items.complete === "undefined") {
-            throw "Did you get everything you need?";
+    function Groceries(name, dueDate, complete) {
+        this.name = name || "eggs";
+        this.dueDate = dueDate || new Date(2013, 07, 05);
+        this.complete = true;
+        this.speak = function() {
+            return this.name + "" + this.date;
+        };
+
+// create a loop that prints out the above object
+
+        for (var i = 0; i < Groceries.length; i++) {
+            try {
+                html += printGroceries(Groceries[i]);
+            } catch (error) {
+                console.log(error);
+            }
         }
-        if (items.complete) {
-            return "<li class='complete'>" + items.name + "</li>";
-        } else if (items.complete === false) {
-            return "<li>" + items.name + "</li>";
+
+// use the object above to create new items
+
+        var milk = new Groceries("milk", false, true);
+        var papertowels = new Groceries("paper towels", false, true);
+        var monkeys = new Groceries("monkeys", new Date(2013, 07, 10), false);
+
+// print out the stuff so it displays in HTML
+
+        html += "</ul>";
+        $('.container').html(html);
+        $(".complete").remove();
+
+// create a function defining printGroceries
+
+        function printGroceries(items) {
+            if (typeof items.name === "undefined") {
+                throw "Aren't you forgetting something?";
+            } else if (typeof items.complete === "undefined") {
+                throw "Did you get everything you need?";
+            }
+            if (items.complete) {
+                return "<li class='complete'>" + items.name + "</li>";
+            } else if (items.complete === false) {
+                return "<li>" + items.name + "</li>";
+            }
         }
-    }
-});
+
+// adding dates to the list
+
+        function weeklyEssentials() {
+            var nextWeek = new Date();
+            nextWeek.setDate(nextWeek.getDate() + 7);
+            var runningOut = new Date();
+            runningOut.setDate(runningOut.getDate() < 7);
+            if (nextWeek.getTime() > runningOut.getTime()) {
+                console.log("Get to the store now! You don't have time to wait.");
+            } else {
+                console.log("You have a week to pick up these items");
+            }
+        }
+    });
+}
