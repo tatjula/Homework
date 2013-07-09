2a67ab144cb84b444650e53ff4543c49e2e328a2
Tatyana <tat@tatyanahjula.com>
2013-07-09 08:19:51 -0400

    resubmitting last week's homework

--- js/assignment_01.js
+++ js/assignment_01.js
@@ -1,50 +1,3 @@
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
 $(document).ready(function() {
     // create an oject within a string, include a name and level of completion
     var groceries = [{
@@ -77,16 +30,17 @@ $(document).ready(function() {
     $('.container').html(html);
     $(".complete").remove();
     // create a function defining printGroceries
+
     function printGroceries(items) {
         if (typeof items.name === "undefined") {
             throw "Aren't you forgetting something?";
         } else if (typeof items.complete === "undefined") {
             throw "Did you get everything you need?";
         }
-        if (items.complete === true) {
-            return ("<li class='complete'>" + items.name + "</li>");
+        if (items.complete) {
+            return "<li class='complete'>" + items.name + "</li>";
         } else if (items.complete === false) {
-            return ("<li>" + items.name + "</li>");
+            return "<li>" + items.name + "</li>";
         }
     }
 });
