037b1ac410565a211e38b01f27762c00685ad926
Tatyana <tat@tatyanahjula.com>
2013-08-12 15:04:18 -0400

    Homework 4: Final Attempt

--- js/assignment_new.js
+++ js/assignment_new.js
@@ -2,58 +2,58 @@ $(document).ready(function() {
 
 // CREATE A BASIC TO-DO LIST WITH AN ARRAY
 
-var groceries = [{
-        "name" : "eggs",
-        "complete" : true,
-        "dueDate": Date(2013, 7, 25)
-    }, {
-        "name" : "milk",
-        "complete" : true,
-        "dueDate": Date(2013, 7, 25)
-    }, {
-        "name" : "bacon",
-        "complete" : true,
-        "dueDate": Date(2013, 7, 25)
-    }, {
-        "name" : "paper towles",
-        "complete" : true,
-        "dueDate": Date(2013, 7, 25)
-    }, {
-        "name" : "monkies",
-        "complete" : false,
-        "dueDate": Date(2015, 7, 25)
-    }];
-
-var container = "<ul>";
-
-for (var i = 0; i < groceries.length; i++) {
-    try {
-        container += listGroceries(groceries[i]);
-    } catch (e) {
-        container =+ "There is a problem!";
-    }
-}
-
-container += "</ul>";
-
-// GENERATE THE HTML
-
-$("div.container").html(container);
-
-function listGroceries (items) {
-    if(typeof items.name === "undefined") {
-        throw "Did you forget something?";
-    } else if (typeof items.complete === "undefined") {
-        throw "Did you get this yet?";
-    }
+var groceryApp = { 
     
-    if(items.complete) {
-        return "<li class='complete'>" + items.name + "</li>";
-    } else {
-        return "<li>" + items.name + "</li>"; 
-    }
-}
+    "listGroceries" : [];
 
-$(".complete").remove();
+    function Groceries(name, complete, dueDate) {
+      this.name = name;
+      this.complete = complete;
+      this.dueDate = Date();
+    }
+
+    var eggs = new Groceries ("eggs", true, Date(2013, 7, 25));
+    var milk = new Groceries ("milk", true, Date(2013, 7, 25));
+    var bacon = new Groceries ("bacon", true, Date(2013, 7, 25));
+    var paperTowles = new Groceries ("paper towles", true, Date(2013, 7, 25));
+    var monkies = new Groceries ("monkies", false, Date(2015, 7, 25));
+
+    // GENERATE THE HTML
+    
+    "gather" : {
+        var groceries = this.Groceries;
+        var container = "<ul>";
+        
+        for (var i = 0; i < groceries.length; i++) {
+            try {
+                container += listGroceries(groceries[i]);
+            } catch (e) {
+                container =+ "There is a problem!";
+            }
+        }
+        
+        container += "</ul>";
+    
+        
+        $("div.container").html(container);
+        
+    },
+    
+    "display" : function listGroceries (items) {
+            if(typeof items.name === "undefined") {
+                throw "Did you forget something?";
+            } else if (typeof items.complete === "undefined") {
+                throw "Did you get this yet?";
+            }
+            
+            if(items.complete) {
+                return "<li class='complete'>" + items.name + "</li>";
+            } else {
+                return "<li>" + items.name + "</li>"; 
+            }
+        },
+        
+    "remove" : $(".complete").remove();
+}
 
 });
